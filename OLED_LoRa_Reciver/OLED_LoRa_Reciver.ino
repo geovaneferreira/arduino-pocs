2cba8db308f0f4443f901af0441e41a84c6e4e3f
Geovane Ferreira <geovanevinicius89@gmail.com>
2021-10-29 14:48:42 -0300

    Correcoes de Bugs
    - A versao original esta quebrada, fiz as correcoes necessarias
    ** OBS so funciona a usb para windows

--- OLED_LoRa_Reciver/OLED_LoRa_Reciver.ino
+++ OLED_LoRa_Reciver/OLED_LoRa_Reciver.ino
@@ -17,7 +17,11 @@ String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
-
+void logo(){
+  display.clear();
+  display.drawXbm(0,5,logo_width,logo_height,logo_bits);
+  display.display();
+}
 
 void loraData(){
   display.clear();
@@ -49,18 +53,19 @@ void setup() {
   Serial.println("LoRa Receiver Callback");
   SPI.begin(SCK,MISO,MOSI,SS);
   LoRa.setPins(SS,RST,DI0);  
-  if (!LoRa.begin(868E6)) {
+  if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-  //LoRa.onReceive(cbk);
-  LoRa.receive();
   Serial.println("init ok");
   display.init();
   display.flipScreenVertically();  
   display.setFont(ArialMT_Plain_10);
-  
+  logo();
   delay(1500);
+  //LoRa.onReceive(cbk);
+  //LoRa.receive();
+ 
 }
 
 void loop() {
